117f3d319bdc5ba20374e0d19751ba1c10812da9
Michela <micheladellagatta1@gmail.com>
2025-11-13 12:32:07 +0100

    Give props to Form component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
+const endpointUrl = "https://67c5b4f3351c081993fb1ab6.mockapi.io/api/posts";
+import Form from "./components/Form";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Form endpoint={endpointUrl} />
+    </>
+  );
 }
 
 export default App;
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
 
-export default function Form() {
+export default function Form({ endpoint }) {
   const defaultFormData = {
     author: "",
     title: "",
