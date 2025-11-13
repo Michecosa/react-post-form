a9f5529a8146ffa0c11c6fe58d50336c427fb2b2
Michela <micheladellagatta1@gmail.com>
2025-11-13 12:30:33 +0100

    Define useState parameters and default value

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,1 +1,13 @@
-export default function Form() {}
+import { useState } from "react";
+import axios from "axios";
+
+export default function Form() {
+  const defaultFormData = {
+    author: "",
+    title: "",
+    body: "",
+    public: false,
+  };
+
+  const [formData, setFormData] = useState(defaultFormData);
+}
