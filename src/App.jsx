e354978792fec6445d76d642818fec240745615e
Michela <micheladellagatta1@gmail.com>
2025-11-13 14:07:20 +0100

    Add Header macro component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 const endpointUrl = "https://67c5b4f3351c081993fb1ab6.mockapi.io/api/posts";
 import Form from "./components/Form";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
       <div className="container">
+        <Header />
         <Form endpoint={endpointUrl} />
       </div>
     </>
---
+++ src/components/Header.jsx
@@ -0,0 +1,3 @@
+export default function Header() {
+  return <h1 className="text-center mt-4">Post form</h1>;
+}
